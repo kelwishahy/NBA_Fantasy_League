688c3e8bfdd7e1327c1a8552c1ab343dea76a89e
Kareem El-Wishahy <kareem.elwishahy@gmail.com>
2020-04-01 21:37:25 -0700

    Initial commit with welcome page

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <style>
+
+    body {
+        background-image: url('./images/background.jpg');
+        background-attachment: fixed;
+        background-size: cover;
+    }
+
+    </style>
+
+    <h1 style="color:white;
+        font-family:Arial;
+        text-align:center;
+        font-size:400%"> Fantasy Basketball </h1>
+
+    <h1 style="color:white;
+        font-family:Arial;
+        text-align:center;
+        font-size:100%"> Developed by Kareem El-Wishahy, Victor Parangue, and Zach Vavasour</h1>
+</html>
+
