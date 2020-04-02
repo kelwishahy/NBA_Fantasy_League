68e977a73a06d8193bf3982d46507392374a9557
Kareem El-Wishahy <kareem.elwishahy@gmail.com>
2020-04-01 22:01:38 -0700

    login text

--- index.php
+++ index.php
@@ -16,12 +16,20 @@
 
     <h1 style="color:white;
         font-family:Arial;
+        font-weight:bold;
         text-align:center;
         font-size:400%"> Fantasy Basketball </h1>
 
     <h1 style="color:white;
         font-family:Arial;
         text-align:center;
-        font-size:100%"> Developed by Kareem El-Wishahy, Victor Parangue, and Zach Vavasour</h1>
+        font-size:100%"> Developed by Kareem El-Wishahy, Victor Parangue, and Zach Vavasour </h1>
+
+    <h2 style="color:white;
+        font-family:Arial;
+        text-align:center;
+        font-weight:bold;
+        font-size:150%;
+        padding-top:70px"> Login </h2>
 </html>
 
