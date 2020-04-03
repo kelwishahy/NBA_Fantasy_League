1e2b9346a79211155d7c1dc4923405d20874b3c5
Kareem El-Wishahy <kareem.elwishahy@gmail.com>
2020-04-03 15:57:28 -0700

    Added chromelogger

--- manageteam.php
+++ manageteam.php
@@ -1,4 +1,7 @@
 <?php
+    //Logger for debugging
+    include "./chromelogger/ChromePhp.php";
+
     //Identify which team is being managed
     if (empty($_COOKIE['teamid'])) {
         header("location: myprofile.php");
@@ -6,32 +9,60 @@
     }
 
     $teamid = $_COOKIE['teamid'];
+    ChromePhp::log("Teamid is $teamid");
 
     //Perform some SQL queries to get all the relevant team info
 
     //Establish database connection
     $db_conn;
     $db_conn = oci_connect("ora_vicp24", "a43444447", "dbhost.students.cs.ubc.ca:1522/stu");
-    console_log("dbconn = $db_conn");
+    ChromePhp::log("dbconn = $db_conn");
 
     $leagueid="";
     $teamname="";
     $totalpoints="";
     $abbrevname="";
 
-    $query_getTeamInfo = "SELECT teamname, totalpoints, league
-                            FROM team
-                            WHERE teamid = '".$teamid."'"
+    $query_getTeamInfo = "SELECT T.teamname, TA.abbrevname, T.totalpoints, T.league 
+                            FROM team T, teamabbreviation TA
+                            WHERE teamid = '".$teamid."'
+                            AND T.teamname = TA.teamname";
+    
+    $statement_getTeamInfo = oci_parse($db_conn, $query_getTeamInfo);
+    $r = oci_execute($statement_getTeamInfo);
+    $nrows = oci_fetch_all($statement_getTeamInfo, $res);
+    ChromePhp::log($res);
+
+    $leagueid = (int)$res['LEAGUE'][0];
+    ChromePhp::log("League ID is $leagueid");
+    $teamname = trim($res['TEAMNAME'][0]);
+    ChromePhp::log("Team name is $teamname");
+    $totalpoints = (int)$res['TOTALPOINTS'][0];
+    ChromePhp::log("Total points: $totalpoints");
+    $abbrevname = trim($res['ABBREVNAME'][0]);
+    ChromePhp::log("Abbreviated name is $abbrevname");
 
 
 
-    //Print to console for debugging purposes
-    function console_log($data) {
-        $output = $data;
-        if (is_array($output))
-            $output = implode(',', $output);
+    //BUTTONS
+    //Logout 
+    if (isset($_POST['logout'])) {
+        //Invalidate the cookie
+        setcookie("username", $username, time()-84000, "/");
 
-        echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+        //Redirect to login page
+        header("location: index.php");
+        exit;
+    }
+
+    //My Profile 
+    if (isset($_POST['profile'])) {
+        //Invalidate the cookie
+        setcookie("teamid", $teamid, time()-84000, "/");
+
+        //Redirect to login page
+        header("location: myprofile.php");
+        exit;
     }
 ?>
 
@@ -56,10 +87,35 @@
     }
     </style>
 
-    <!-- <div class="transparent-overlay">
+    <div class="transparent-overlay">
         <h1 style="color:white;
-            font-weight:bold;
-            text-align:center;
-            font-size:400%;"> <?//php echo $username; ?>'s Profile </h1> -->
+                font-weight:bold;
+                text-align:center;
+                font-size:400%;"> <?php echo $teamname; ?> (<?php echo $abbrevname; ?>) </h1>
+
+    <form action="" method="POST">
+            <input type="submit" name="logout" value="Logout"
+            style="background-color:#fc9803;
+            color:white; 
+            border:none;
+            top: 0;
+            right: 0;
+            position: absolute;
+            margin-top: 10px;
+            margin-right: 10px;
+            "></input>
+        </form>
+    <form action="" method="POST">
+            <input type="submit" name="profile" value="My Profile"
+            style="background-color:#fc9803;
+            color:white; 
+            border:none;
+            top: 0;
+            right: 0;
+            position: absolute;
+            margin-top: 30px;
+            margin-right: 10px;
+            "></input>
+        </form>
     </div>
 </html>
--- myprofile.php
+++ myprofile.php
@@ -1,17 +1,41 @@
 <?php
+    //Logger for debugging
+    include "./chromelogger/ChromePhp.php";
+
     if (empty($_COOKIE['username'])) {
         header("location: index.php");
         exit;
     }
 
     $username = $_COOKIE['username'];
-    
-    console_log("$username");
+    ChromePhp::log("$username");
 
+    //Connect to the database
+    $db_conn;
+    $db_conn = oci_connect("ora_vicp24", "a43444447", "dbhost.students.cs.ubc.ca:1522/stu");
+
+    //Get teamid and set it in a cookie
+    $teamid="";
+    $query_getTeamID = "SELECT teamid
+                        FROM teamownedby
+                        WHERE leagueparticipantid = '".$username."'
+                        ";
+    
+    $statement_getTeamID = oci_parse($db_conn, $query_getTeamID);
+    $r = oci_execute($statement_getTeamID);
+    $nrows = oci_fetch_all($statement_getTeamID, $res);
+    ChromePhp::log("User owns $nrows team(s)");
+
+    ChromePhp::log($res);
+    $teamid = $res["TEAMID"][0];
+    ChromePhp::log($teamid);
+    ChromePhp::log("Selected teamid $teamid");
+
+
+    //Logout functionality
     if (isset($_POST['logout'])) {
         logout();
     }
-
 
     function logout() {
         //Invalidate the cookie
@@ -22,16 +46,15 @@
         exit;
     }
 
+    //Navigate to team manager
+    if (isset($_POST['manage'])) {
+        //Create a cookie
+        setcookie("teamid", $teamid, time()+1800, "/");
 
-    //Print to console for debugging purposes
-    function console_log($data) {
-        $output = $data;
-        if (is_array($output))
-            $output = implode(',', $output);
-
-        echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+        //Redirect to team manager page
+        header("location: manageteam.php");
+        exit;
     }
-
 ?>
 
 <!DOCTYPE html>
@@ -73,5 +96,19 @@
             margin-right: 10px;
             "></input>
         </form>
+
+        <form action="" method="POST">
+            <input type="submit" name="manage" value="Manage Team"
+            style="background-color:#fc9803;
+            color:white; 
+            border:none;
+            top: 0;
+            right: 0;
+            position: absolute;
+            margin-top: 30px;
+            margin-right: 10px;
+            "></input>
+        </form>
+
     </div>
 </html>
